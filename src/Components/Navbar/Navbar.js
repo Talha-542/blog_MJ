f649d7d0d22d2a1469a02aae82ba0a8dcd72f2a7
Talha-542 <talha-542@users.noreply.github.com>
2024-08-28 14:34:19 +0500

    navbar , header , top categories (under development)

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Navbar.css'
+export default function Navbar() {
+  return (
+    <div>
+      <p className='firstname'>Micheal</p><span className='lastname'>Johnstone</span>
+    </div>
+  )
+}
