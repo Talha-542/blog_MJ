df5ae245aca50442c14b2be0b9d08b027deae8cd
Talha-542 <ta662561@gmail.com>
2024-08-30 14:42:33 +0500

    Pagination in New UI

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import TopCategories from '../Components/TopCategories/TopCategories';
 import Header from '../Components/Header/Header';
 import Posts from '../Components/Posts/Posts';
+import Paginatio from '../Components/Pagination/Pagination'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Header/>
       <TopCategories/>
       <Posts/>
+      <Paginatio/>
     </div>
   )
 }
