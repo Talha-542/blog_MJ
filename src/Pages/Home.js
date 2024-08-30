90a7aa77c11a4c575b0e2b13d09d65726b5d37d8
Talha-542 <ta662561@gmail.com>
2024-08-30 14:41:13 +0500

    Enhanced UI

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,14 +2,13 @@ import React from 'react'
 import TopCategories from '../Components/TopCategories/TopCategories';
 import Header from '../Components/Header/Header';
 import Posts from '../Components/Posts/Posts';
-// import Pagination form '../Components/Pagination?/Pagination ';
+
 export default function Home() {
   return (
     <div>
       <Header/>
       <TopCategories/>
       <Posts/>
-    // <Pagination/> 
     </div>
   )
 }
