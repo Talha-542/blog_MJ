d6fbaa3f6b15a456663ff20ab91f92fa1ab2f29c
Talha-542 <ta662561@gmail.com>
2024-08-31 18:09:34 +0500

    All Category page (started functioning)

--- src/Components/TopCategories/TopCategories.js
+++ src/Components/TopCategories/TopCategories.js
@@ -1,9 +1,8 @@
-import React, { useContext } from 'react';
-import { StoreContext } from '../../store';
-import article_count_logo from '../../assets/article_count_logo.svg'
-import styles from './TopCategories.module.css';
-import {Link} from 'react-router-dom'
-
+import React, { useContext } from "react";
+import { StoreContext } from "../../store";
+import article_count_logo from "../../assets/article_count_logo.svg";
+import styles from "./TopCategories.module.css";
+import { Link } from "react-router-dom";
 
 const TopCategories = () => {
   const { category_list } = useContext(StoreContext);
@@ -12,15 +11,26 @@ const TopCategories = () => {
     <div className={styles.container}>
       <div className={styles.header}>
         <h2 className={styles.title}>Top Categories</h2>
-        <Link to="/all-categories" className={styles.seeAll}>See all</Link>      </div>
+        <Link to="/all-categories" className={styles.seeAll}>
+          See all
+        </Link>
+      </div>
       <div className={styles.categories}>
-        {category_list.map(category => (
+        {category_list.map((category) => (
           <div key={category.id} className={styles.categoryCard}>
-            <img src={category.image} alt={category.name} className={styles.categoryImage} />
+            <img
+              src={category.image}
+              alt={category.name}
+              className={styles.categoryImage}
+            />
             <div className={styles.articleContent}>
               <div className={styles.categoryName}>{category.name}</div>
               <p className={styles.articleCount}>
-                <img src={article_count_logo} alt="Article count icon" className={styles.articleCountIcon} />
+                <img
+                  src={article_count_logo}
+                  alt="Article count icon"
+                  className={styles.articleCountIcon}
+                />
                 {category.count}
               </p>
             </div>
