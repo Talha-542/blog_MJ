47b62df6c4ebb1e1f93e11701112ba15aaeb6e82
Talha-542 <ta662561@gmail.com>
2024-09-04 23:27:51 +0500

    back button glitch (fixed)

--- src/Pages/AllCategories/AllCategories.js
+++ src/Pages/AllCategories/AllCategories.js
@@ -1,56 +1,44 @@
-import React,{ useContext } from 'react'
-import { Link } from 'react-router-dom'
-import styles  from './AllCategories.module.css'
-import { StoreContext } from "../../store" 
-import article_count_logo from "../../assets/article_count_logo.svg" 
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import styles from "./AllCategories.module.css";
+import { StoreContext } from "../../store";
+import article_count_logo from "../../assets/article_count_logo.svg";
 
 export default function AllCategories() {
-  const { Allcategory_list } = useContext(StoreContext) 
+  const { Allcategory_list } = useContext(StoreContext);
   return (
-   <div>
-   <div className={styles.header}>
-      <div className={styles.backContainer}>
-      <Link to="/" className={styles.back}>
+    <div>
+      <div className={styles.header}>
+        <Link to="/" className={styles.back}>
           Back
-      </Link>
+        </Link>
+        <div className={styles.title}>Categories</div>
       </div>
-      <div className={styles.title}>
-      Categories
-      </div>
-    </div>
 
-    <div className={styles.postcontainer}>
-    <div className={styles.categories}>
-      {Allcategory_list.map((category) => (
-        <div key={category.id} className={styles.categoryCard}>
-          <img
-            src={category.image}
-            alt={category.name}
-            className={styles.categoryImage}
-          />
-          <div className={styles.articleContent}>
-            <div className={styles.categoryName}>{category.name}</div>
-            <p className={styles.articleCount}>
+      <div className={styles.postcontainer}>
+        <div className={styles.categories}>
+          {Allcategory_list.map((category) => (
+            <div key={category.id} className={styles.categoryCard}>
               <img
-                src={article_count_logo}
-                alt="Article count icon"
-                className={styles.articleCountIcon}
+                src={category.image}
+                alt={category.name}
+                className={styles.categoryImage}
               />
-              {category.count}
-            </p>
-          </div>
+              <div className={styles.articleContent}>
+                <div className={styles.categoryName}>{category.name}</div>
+                <p className={styles.articleCount}>
+                  <img
+                    src={article_count_logo}
+                    alt="Article count icon"
+                    className={styles.articleCountIcon}
+                  />
+                  {category.count}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
-  </div>
-  </div>
-) 
-
+  );
 }
-
-
-
- 
-
- 
-
